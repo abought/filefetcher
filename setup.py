982cfde75016e7889ad8b02daec20c8ea048e7cf
Andy Boughton <abought@gmail.com>
2020-04-14 13:38:49 -0400

    Bump for v0.1.0 (initial release)

--- setup.py
+++ setup.py
@@ -11,11 +11,26 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='filefetcher',
     version='0.1.0',
-    packages=find_packages(exclude=['docs', 'tests']),
-    url='',
+    packages=find_packages(exclude=['docs', 'tests', 'venv']),
+    url='https://github.com/abought/filefetcher',
     license='',
     author='abought',
     author_email='abought@umich.edu',
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+    ],
+
     description='Find, download, and build versioned assets',
     long_description=long_description,
     long_description_content_type='text/markdown',  # Optional (see note above)
@@ -23,5 +38,10 @@ setup(
     # install_requires=[],
     extras_require={  # Optional
         'test': ['coverage', 'pytest', 'pytest-flake8'],
+    },
+
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/abought/filefetcher/issues',
+        'Source': 'https://github.com/abought/filefetcher/',
     },
 )
