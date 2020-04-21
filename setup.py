51b4c862dbfd60dde935b6a93ecd0b91ff8d980e
Andy Boughton <abought@gmail.com>
2020-04-21 12:45:30 -0400

    Bump version for 0.1.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='filefetcher',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(exclude=['docs', 'tests', 'venv']),
     url='https://github.com/abought/filefetcher',
     license='',
